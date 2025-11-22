3a6b01ccedd0f288339226a448f9ad7bf1502467
Aljel <makakwar@gmail.com>
2025-11-22 13:48:08 +0400

    fix: some strange shit

--- backend/internal/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// User defines the structure for a user.
-type User struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
